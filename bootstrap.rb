3a42dd7e94f45aaa881215df005f3b9f5e8b85b6
Rob Szumski <rob@robszumski.com>
2014-01-23 21:47:23 -0800

    refactor

--- bootstrap.rb
+++ bootstrap.rb
@@ -19,142 +19,157 @@ hostname = nil
 port = nil
 serverId = nil
 parsedHostname = nil
+#http.set_debug_output($stdout)
 
+# Read command line flags
 parser = OptionParser.new do|opts|
   opts.banner = "Usage: bootstrap.rb [options]"
   opts.on('-h', '--hostname http://ip-or-hostname', 'Hostname') do |hostname|
     parsedHostname = URI.parse(hostname)
     puts "COMMAND: Parsed hostname is #{parsedHostname.to_s}"
-    #hostname = parsedHostname.host
-    #port = parsedHostname.port
   end
 end
 parser.parse!
 
-hostname = parsedHostname.host
-port = parsedHostname.port
+#hostname = parsedHostname.host
+#port = parsedHostname.port
 
 # Generate user/pass
-username_base = "admin"
-username_random = ('a'..'z').to_a.shuffle[0,8].join
-username = username_base + "_" + username_random
-password = SecureRandom.hex
-encrypted_password = password; #encrypt this!
+def generateUsername
+  username_base = "admin"
+  username_random = ('a'..'z').to_a.shuffle[0,8].join
+  username = username_base + "_" + username_random
+end
+generateUsername
+
+def generatePassword
+  password = SecureRandom.hex
+  encrypted_password = password; #encrypt this!
+end
+generatePassword
 
 # Initialize http
 http = Net::HTTP.new("172.17.42.1", 4001)
 
 # Save to etcd
-#http.set_debug_output($stdout)
-registerRequest = Net::HTTP::Put.new("/v2/keys/services/buildafund-mysql/instances/#{hostname}:#{port}")
-registerRequest.set_form_data('dir' => 'true', 'ttl' => 60)
-registerResponse = http.request(registerRequest);
-case registerResponse.code
-  when "201"
-    puts "REGISTER: register success"
-  when "403"
-    puts "REGISTER: Already registered. Updating TTL"
-    generateCredentials = false;
-    registerRequest = Net::HTTP::Put.new("/v2/keys/services/buildafund-mysql/instances/#{hostname}:#{port}")
-    registerRequest.set_form_data('prevExist' => 'true', 'dir' => 'true', 'ttl' => 60)
-    registerResponse = http.request(registerRequest);
-  when "404"
-    puts "REGISTER: Could not register. Received 404 from etcd"
-  else
-    puts "REGISTER: Could not register. Received unknown code #{registerResponse.code} from etcd"
+def register (hostname, port)
+  registerRequest = Net::HTTP::Put.new("/v2/keys/services/buildafund-mysql/instances/#{hostname}:#{port}")
+  registerRequest.set_form_data('dir' => 'true', 'ttl' => 60)
+  registerResponse = http.request(registerRequest);
+  case registerResponse.code
+    when "201"
+      puts "REGISTER: register success"
+    when "403"
+      puts "REGISTER: Already registered. Updating TTL"
+      generateCredentials = false;
+      registerRequest = Net::HTTP::Put.new("/v2/keys/services/buildafund-mysql/instances/#{hostname}:#{port}")
+      registerRequest.set_form_data('prevExist' => 'true', 'dir' => 'true', 'ttl' => 60)
+      registerResponse = http.request(registerRequest);
+    when "404"
+      puts "REGISTER: Could not register. Received 404 from etcd"
+    else
+      puts "REGISTER: Could not register. Received unknown code #{registerResponse.code} from etcd"
+  end
 end
+register(parsedHostname.host, parsedHostname.port)
 
-# Generate new creds if needed
-if generateCredentials
-  # Write username to etcd
+def etcdWrite (keyPath, value, comment)
   writeRequest = Net::HTTP::Put.new("/v2/keys/services/buildafund-mysql/instances/#{hostname}:#{port}/user")
   writeRequest.set_form_data('value' => username)
   writeResponse = http.request(writeRequest);
   case writeResponse.code
     when "201", "200"
-      puts "WRITE: Username #{username} written successfully."
+      puts "WRITE: #{comment} written successfully."
     else
       puts "WRITE: Could not write username. Received unknown code #{writeResponse.code} from etcd"
   end
+end
 
-    # Write password to etcd
-  passwordRequest = Net::HTTP::Put.new("/v2/keys/services/buildafund-mysql/instances/#{hostname}:#{port}/password")
-  passwordRequest.set_form_data('value' => password)
-  passwordResponse = http.request(passwordRequest);
-  case passwordResponse.code
-    when "201", "200"
-      puts "WRITE: Password written successfully."
-    else
-      puts "WRITE: Could not write password. Received unknown code #{passwordResponse.code} from etcd"
-  end
+# Generate new creds if needed
+if generateCredentials
+  # Write Username
+  path = "/v2/keys/services/buildafund-mysql/instances/#{hostname}:#{port}/user"
+  etcdWrite(path, username, "Username #{username}")
+  # Write Password
+  path = "/v2/keys/services/buildafund-mysql/instances/#{hostname}:#{port}/password"
+  etcdWrite(path, username, "Password")
 else
   puts "WRITE: Credentials already exist. Skipping generation."
 end
 
 # Read current master
-leaderRequest = Net::HTTP::Get.new("/mod/v2/leader/buildafund-mysql")
-leaderResponse = http.request(leaderRequest)
-case
-  when leaderResponse.code.eql?("404")
-    # this should work. etcd is returning the wrong status code
-  when leaderResponse.body.include?("get leader error: read lock error: Cannot reach servers after 3 time")
-    attemptElection = true
-    puts "ELECTION: Encountered 'get leader error'"
-    puts "ELECITON: Leader not found. Attempting Election"
-  when leaderResponse.body.empty?
-    attemptElection = true
-    puts "ELECTION: Encountered empty body"
-    puts "ELECTION: Leader not found. Attempting Election"
-  else
-    currentLeader = "http://" + leaderResponse.body.to_s
-    parsedLeader = URI.parse(currentLeader)
-    puts "ELECTION: Elected leader is #{parsedLeader.to_s}"
-    currentLeaderHost = parsedLeader.host.to_s
-    currentLeaderPort = parsedLeader.port.to_s
-    currentLeader = "#{currentLeaderHost}:#{currentLeaderPort}"
+def readLeader (etcdPath)
+  leaderRequest = Net::HTTP::Get.new(etcdPath)
+  leaderResponse = http.request(leaderRequest)
+  leaderValue = nil
+  case
+    when leaderResponse.code.eql?("404")
+      # this should work. etcd is returning the wrong status code
+    when leaderResponse.body.include?("get leader error: read lock error: Cannot reach servers after 3 time")
+      puts "ELECTION: Encountered 'get leader error'"
+      puts "ELECITON: Leader not found. Attempting Election"
+    when leaderResponse.body.empty?
+      puts "ELECTION: Encountered empty body"
+      puts "ELECTION: Leader not found. Attempting Election"
+    else
+      leaderString = "http://" + leaderResponse.body.to_s
+      parsedLeader = URI.parse(leaderString)
+      puts "ELECTION: Elected leader is #{parsedLeader.to_s}"
+      leaderValue = Hash.new
+      leaderValue['host'] = parsedLeader.host.to_s
+      leaderValue['port'] = parsedLeader.port.to_s
+      leaderValue['full'] = "#{parsedLeader.host}:#{parsedLeader.port}"
+  end
+  return leaderValue
 end
+path = "/mod/v2/leader/buildafund-mysql"
+currentLeader = readLeader(path)
 
 # Attempt to become master
-if attemptElection
-  electionRequest = Net::HTTP::Put.new("/mod/v2/leader/buildafund-mysql?ttl=60")
-  electionRequest.set_form_data('name' => "#{hostname.to_s}:#{port.to_s}")
-  electionResponse = http.request(electionRequest)
-  case electionResponse.code
-    when "200"
-      puts "ELECTION: Election successful. #{hostname.to_s}:#{port.to_s} is now the master."
-      currentLeader = "#{hostname.to_s}:#{port.to_s}"
-      currentLeaderHost = hostname.to_s
-      currentLeaderPort = port.to_s
-  end
+def becomeLeader(etcdPath, value)
+  electionRequest = Net::HTTP::Put.new(etcdPath)
+    electionRequest.set_form_data('name' => value)
+    electionResponse = http.request(electionRequest)
+    case electionResponse.code
+      when "200"
+        puts "ELECTION: Election successful. #{value} is now the master."
+        return true
+    end
+end
+
+if currentLeader.nil?
+  path = "/mod/v2/leader/buildafund-mysql?ttl=60"
+  isNewLeader = becomeLeader(path, "#{currentLeader['full']}")
 end
 
 # Read all instances
-#http.set_debug_output($stdout)
-instancesRequest = Net::HTTP::Get.new("/v2/keys/services/buildafund-mysql/instances?recursive=true")
-instancesResponse = http.request(instancesRequest)
-instances = JSON.parse(instancesResponse.body)
+def etcdRead(etcdPath)
+  instancesRequest = Net::HTTP::Get.new(etcdPath)
+  instancesResponse = http.request(instancesRequest)
+  instances = JSON.parse(instancesResponse.body)
 
-slaveDetails = Hash.new
-leaderDetails = Hash.new
-instances['node']['nodes'].each do |instance|
-  name = instance['key'].split('/')[-1]
-  serverId = Integer(instance['createdIndex'])
-  keyData = Hash.new
-  instance['nodes'].each do |keys|
-    keyName = keys['key'].split('/')[-1]
-    keyValue = keys['value'].chomp('"').reverse.chomp('"').reverse #remove quotes, crazy
-    keyData[keyName] = keyValue
-    if !currentLeader.eql?(name)
-      slaveDetails[name] = keyData
-    else
-      leaderDetails[name] = keyData
+  instanceDetails = Hash.new
+  instances['node']['nodes'].each do |instance|
+    name = instance['key'].split('/')[-1]
+    serverId = Integer(instance['createdIndex'])
+    keyData = Hash.new
+    instance['nodes'].each do |keys|
+      keyName = keys['key'].split('/')[-1]
+      keyValue = keys['value'].chomp('"').reverse.chomp('"').reverse #remove quotes, crazy
+      keyData[keyName] = keyValue
+      instanceDetails[name] = keyData
     end
+    return instanceDetails
   end
 end
-#puts "slave:"
-#pp slaveDetails
-#puts "leader:"
-#pp leaderDetails
+
+instances = etcdRead("/v2/keys/services/buildafund-mysql/instances?recursive=true")
+instances.each_with_index do |name, data|
+  if currentLeader["full"].eql?(name)
+    currentLeader["user"] = data["user"]
+    currentLeader["password"] = data["password"]
+  end
+end
 
 # Write config file based on role
 puts "----------------------------Generated Config--------------------------------"
@@ -165,7 +180,7 @@ File.open('/etc/mysql/conf.d/replication.cnf', 'w') { |file| file.write(cnf.resu
 puts "-------------------------------End Config-----------------------------------"
 
 # Start mysql
-#=begin
+=begin
 puts "MYSQL: Installing DB"
 installMysql = `mysql_install_db`
 puts "MYSQL: Starting process"
@@ -186,4 +201,4 @@ if !"#{hostname}:#{port}".eql?(currentLeader)
 else
   puts "MASTER: No configuration was needed."
 end
-#=end
+=end
